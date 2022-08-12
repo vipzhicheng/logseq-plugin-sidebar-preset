06f7f7fd25c64f06b50ab8e38a8b3473fa0133d1
Richard Yu <vipzhicheng@gmail.com>
2022-08-12 12:06:19 +0800

    add readme

--- README.md
+++ README.md
@@ -4,6 +4,20 @@
 
 A Logseq plugin to reset a managed sidebar.
 
+![Screencast](./screencast.gif)
+
+Logseq's sidebar is a dynamic region, sometimes you want to keep some pages or blocks always shown on top and in specific order. This plugin can help you do this.
+
+It provides you five lists (I hope it's enough for you), you can choose to use one of them.
+
+## Known issue
+
+For now it can not clear those non-used panes, but you can use shortcut: double `cmd+c` to clear sidebar first, then click the reset sidebar toolbar icon.
+
+## Buy me a coffee
+
+If my plugin solve your situation a little bit and you will, you can choose to buy me a coffee via [this](https://www.buymeacoffee.com/vipzhicheng) and [this](https://afdian.net/@vipzhicheng).
+
 
 ## Licence
 MIT
--- src/main.ts
+++ src/main.ts
@@ -2,20 +2,111 @@ import "@logseq/libs";
 import { SettingSchemaDesc } from "@logseq/libs/dist/LSPlugin.user";
 
 const defineSettings: SettingSchemaDesc[] = [
+  // @ts-ignore
   {
-    key: "sidebar_content",
-    title: "Sidebar Content",
-    description: "Sidebar Content",
-    default: "",
+    key: "generalHeading",
+    title: "General settings",
+    type: "heading",
+  },
+  {
+    key: "chooseSidebar",
+    title: "Choose sidebar",
+    description: "Choose sidebar",
+    type: "enum",
+    enumPicker: "select",
+    enumChoices: ["sidebar1", "sidebar2", "sidebar3", "sidebar4", "sidebar5"],
+    default: "sidebar1",
+  },
+  // @ts-ignore
+  {
+    key: "sidebarlHeading",
+    title: "Sidebars",
+    type: "heading",
+  },
+  {
+    key: "sidebar1",
+    title: "Sidebar 1",
+    description: "Sidebar 1",
     type: "string",
-    inputAs: "range",
+    inputAs: "textarea",
+    default: "",
+  },
+  {
+    key: "sidebar2",
+    title: "Sidebar 2",
+    description: "Sidebar 2",
+    type: "string",
+    inputAs: "textarea",
+    default: "",
+  },
+  {
+    key: "sidebar3",
+    title: "Sidebar 3",
+    description: "Sidebar 3",
+    type: "string",
+    inputAs: "textarea",
+    default: "",
+  },
+  {
+    key: "sidebar4",
+    title: "Sidebar 4",
+    description: "Sidebar 4",
+    type: "string",
+    inputAs: "textarea",
+    default: "",
+  },
+  {
+    key: "sidebar5",
+    title: "Sidebar 5",
+    description: "Sidebar 5",
+    type: "string",
+    inputAs: "textarea",
+    default: "",
   },
 ];
-
 logseq.useSettingsSchema(defineSettings);
 
 const main = async () => {
-  logseq.App.showMsg("Hello World!");
+  const model = {
+    async resetSidebar() {
+      if (logseq.settings?.chooseSidebar) {
+        const sidebar = logseq.settings.chooseSidebar;
+        const sidebarContent = logseq.settings[sidebar];
+        if (sidebarContent) {
+          const sidebarSplit = sidebarContent.split("\n").reverse();
+
+          for (let piece of sidebarSplit) {
+            if (piece.startsWith("((")) {
+              const uuid = piece.replace(/^\(+/, "").replace(/\)+$/, "");
+              const block = await logseq.Editor.getBlock(uuid);
+              if (block) {
+                logseq.App.openInRightSidebar(block.uuid);
+              }
+            } else {
+              const pageName = piece.replace(/^\[+/, "").replace(/\]+$/, "");
+              const page = await logseq.Editor.getPage(pageName);
+              if (page) {
+                logseq.App.openInRightSidebar(page.uuid);
+              }
+            }
+          }
+        }
+      }
+    },
+  };
+
+  logseq.provideModel(model);
+  logseq.App.registerUIItem("toolbar", {
+    key: "logseq-reset-sidebar",
+    template: `
+      <a class="button" data-on-click="resetSidebar" title="Reset sidebar">
+      <i class="ti ti-layout-sidebar-right" style=""></i>
+      </a>
+    `,
+  });
+  logseq.onSettingsChanged(() => {
+    model.resetSidebar();
+  });
 };
 
 logseq.ready(main).catch(console.error);
